513e0e4ec18fed0ea61ff17d09944f5c2c086a2d
Yatin Gupta <yatingupta7777@gmail.com>
2022-10-20 20:06:58 -0500

    update seeds with fav movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 # Seed the RottenPotatoes DB with some movies.
 more_movies = [
-  {:title => 'My Neighbor Totoro', :rating => 'G',
-    :release_date => '16-Apr-1988'},
-  {:title => 'Green Book', :rating => 'PG-13',
-    :release_date => '16-Nov-2018'},
-  {:title => 'Parasite', :rating => 'R',
-    :release_date => '30-May-2019'},
-  {:title => 'Nomadland', :rating => 'R',
-    :release_date => '19-Feb-2021'},
-  {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+  {:title => 'Perks of being a Wallflower', :rating => 'PG-13',
+    :release_date => '21-Sep-2012'},
+  {:title => 'Interstellar', :rating => 'PG-13',
+    :release_date => '26-Oct-2014'},
+  {:title => '500 days of summer', :rating => 'PG-13',
+    :release_date => '17-July-2009'},
+  {:title => 'Shutter Island ', :rating => 'R',
+    :release_date => '19-Feb-2010'},
+  {:title => 'Jojo Rabbit', :rating => 'PG-13',
+    :release_date => '08-Nov-2019'}
 ]
 
 more_movies.each do |movie|
